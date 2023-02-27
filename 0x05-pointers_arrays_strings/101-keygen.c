fb3c8a0b1080b14ce7d4b7ea50732260cc65be3e
Jese Nicolle <entity01@pm.me>
2023-02-27 01:21:49 -0800

    haha

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -31,22 +31,6 @@ int main(void)
 
 	}
 
-	password[0] = 'T';
-	password[1] = 'a';
-	password[2] = 'd';
-	password[3] = 'a';
-	password[4] = '!';
-	password[5] = 'C';
-	password[6] = 'o';
-	password[7] = 'n';
-	password[8] = 'g';
-	password[9] = 'r';
-	password[10] = 'a';
-	password[11] = 't';
-	password[12] = 's';
-	password[13] = '\n';
-	password[14] = '\0';
-
 	printf("%s", password);
 	return 0;
 }
