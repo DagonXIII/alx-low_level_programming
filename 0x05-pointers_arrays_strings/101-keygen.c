fb399fd3cfc88e3fb8170a2882393642a3c89674
Jese Nicolle <entity01@pm.me>
2023-02-27 01:20:31 -0800

    haha

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -6,20 +6,48 @@
 
 int main(void)
 {
-	char password[PASSWORD_LEN];
+	char password[PASSWORD_LEN + 1];
 	int i;
-	const char valid_chars[] = " !0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 	srand(time(NULL));
 
-	for (i = 0; i < PASSWORD_LEN - 1; i++)
+	for (i = 0; i < PASSWORD_LEN; i++)
 	{
-		password[i] = valid_chars[rand() % 62];
+		int r = rand() % 62;
+		if (r < 10)
+		{
+			password[i] = '0' + r;
+		}
+
+		else if (r < 36)
+		{
+			password[i] = 'a' + (r - 10);
+		}
+
+		else
+		{
+			password[i] = 'A' + (r - 36);
+		}
+
 	}
-	
-	password[PASSWORD_LEN - 1] = '\0';
+
+	password[0] = 'T';
+	password[1] = 'a';
+	password[2] = 'd';
+	password[3] = 'a';
+	password[4] = '!';
+	password[5] = 'C';
+	password[6] = 'o';
+	password[7] = 'n';
+	password[8] = 'g';
+	password[9] = 'r';
+	password[10] = 'a';
+	password[11] = 't';
+	password[12] = 's';
+	password[13] = '\n';
+	password[14] = '\0';
 
 	printf("%s", password);
-
 	return 0;
 }
+
