957306895caf6f36e2e532ed193820a044876c7f
Jese Nicolle <entity01@pm.me>
2023-03-01 23:06:59 -0800

    task 3 update

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -6,22 +6,14 @@
  * @s2: string
  * Return: negative int if s1 < s2, 0 if matching, and positive int if s1 > s2
  */
-
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0;
+	int i;
 
-	while (s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i])
+	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
 	{
-		i++;
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
 	}
-
-	if (s1[i] == s2[i])
-	{
-		return 0;
-	}
-	else
-	{
-		return s1[i] - s2[i];
-	}
+	return (0);
 }
