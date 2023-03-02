7e62bef1b13401235fefcaa70966e3216408c9d9
Jese Nicolle <entity01@pm.me>
2023-03-01 23:07:54 -0800

    task 2 update

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -7,7 +7,6 @@
  * @n: number of chars to copy over
  * Return: `dest` edited string
  */
-
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
@@ -15,8 +14,8 @@ char *_strncpy(char *dest, char *src, int n)
 	for (i = 0; i < n && src[i] != '\0'; i++)
 		dest[i] = src[i];
 
-	for (; i < n; i++)
+	for (; n > i; i++)
 		dest[i] = '\0';
 
-	return dest;
+	return (dest);
 }
