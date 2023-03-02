53c92ea37a64457fbdf6401edf9f0aedaa756f02
Jese Nicolle <entity01@pm.me>
2023-03-01 23:16:55 -0800

    task 0 update

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,8 +2,8 @@
 
 /**
  * _strcat - concatenate two strings
- * dest: destination string
- * src: source string
+ * @dest: destination string
+ * @src: source string
  * Return: pointer to resulting string dest
  */
 char *_strcat(char *dest, char *src)
