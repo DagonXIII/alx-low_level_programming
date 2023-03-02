baf3475ce0c7815f8e7db9f48e79aed227d12bec
Jese Nicolle <entity01@pm.me>
2023-03-02 02:01:08 -0800

    task 0 update

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -11,52 +11,37 @@
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int len1 = 0, len2 = 0, carry = 0, i, j, sum;
-	char *tmp, *result;
+	int i, j, k, l, m, n;
 
-	/* Calculate lengths of input strings */
-	while (n1[len1] != '\0')
-		len1++;
-	while (n2[len2] != '\0')
-		len2++;
-
-	/* Check if result can fit in buffer */
-	if (len1 + 1 > size_r || len2 + 1 > size_r)
+	for (i = 0; n1[i]; i++)
+		;
+	for (j = 0; n2[j]; j++)
+		;
+	if (i > size_r || j > size_r)
 		return (0);
-
-	/* Allocate memory for temporary string */
-	tmp = malloc((size_r + 1) * sizeof(char));
-	if (tmp == NULL)
-		return (0);
-
-	/* Add digits from right to left */
-	for (i = len1 - 1, j = len2 - 1; i >= 0 || j >= 0 || carry > 0; i--, j--)
+	m = 0;
+	for (i -= 1, j -= 1, k = 0; k < size_r - 1; i--, j--, k++)
 	{
-		sum = carry;
+		n = m;
 		if (i >= 0)
-			sum += n1[i] - '0';
+			n += n1[i] - '0';
 		if (j >= 0)
-			sum += n2[j] - '0';
-
-		if (sum > 9)
+			n += n2[j] - '0';
+		if (i < 0 && j < 0 && n == 0)
 		{
-			carry = 1;
-			sum -= 10;
+			break;
 		}
-		else
-			carry = 0;
-
-		tmp[len1 - i - 1] = sum + '0';
+		m = n / 10;
+		r[k] = n % 10 + '0';
 	}
-
-	/* Reverse the temporary string and copy to result buffer */
-	result = r;
-	for (i = len1 - 1, j = 0; i >= 0; i--, j++)
-		result[j] = tmp[i];
-	result[j] = '\0';
-
-	/* Free temporary memory */
-	free(tmp);
-
-	return (result);
+	r[k] = '\0';
+	if (i >= 0 || j >= 0 || m)
+		return (0);
+	for (k -= 1, l = 0; l < k; k--, l++)
+	{
+		m = r[k];
+		r[k] = r[l];
+		r[l] = m;
+	}
+	return (r);
 }
