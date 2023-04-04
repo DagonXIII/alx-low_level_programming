01acfad9e9d52c356bdb41bf922d8ca2039147ea
Jese Nicolle <entity01@pm.me>
2023-04-03 23:31:05 -0700

    should now free

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -3,15 +3,16 @@
 /**
  * free_listint - Frees a listint_t list
  * @head: Pointer to the head of the list
+ * Rturn: Null in case of error
  */
 void free_listint(listint_t *head)
 {
-    listint_t *tmp;
+	listint_t *freeit;
 
-    while (head != NULL)
-    {
-        tmp = head;
-        head = head->next;
-        free(tmp);
-    }
+	while (head != NULL)
+	{
+		freeit = head;
+		head = head->next;
+		free(freeit);
+	}
 }
