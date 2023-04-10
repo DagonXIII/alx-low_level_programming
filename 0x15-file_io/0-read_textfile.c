b2ce5b897ab79bfec1adac57a213394af558d2f1
Jese Nicolle <entity01@pm.me>
2023-04-10 09:33:54 -0700

    betty

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,38 +9,38 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    ssize_t bytes_read = 0, bytes_written = 0;
-    char *buffer = NULL;
-    int file_descriptor = 0;
+	ssize_t bytes_read = 0, bytes_written = 0;
+	char *buffer = NULL;
+	int file_descriptor = 0;
 
-    if (filename == NULL)
-        return (0);
+	if (filename == NULL)
+		return (0);
 
-    file_descriptor = open(filename, O_RDONLY);
-    if (file_descriptor == -1)
-        return (0);
+	file_descriptor = open(filename, O_RDONLY);
+	if (file_descriptor == -1)
+		return (0);
 
-    buffer = malloc(sizeof(char) * letters);
-    if (buffer == NULL)
-        return (0);
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+		return (0);
 
-    bytes_read = read(file_descriptor, buffer, letters);
-    if (bytes_read == -1)
-    {
-        free(buffer);
-        close(file_descriptor);
-        return (0);
-    }
+	bytes_read = read(file_descriptor, buffer, letters);
+	if (bytes_read == -1)
+	{
+		free(buffer);
+		close(file_descriptor);
+		return (0);
+	}
 
-    bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
-    if (bytes_written == -1 || bytes_written != bytes_read)
-    {
-        free(buffer);
-        close(file_descriptor);
-        return (0);
-    }
+	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+	if (bytes_written == -1 || bytes_written != bytes_read)
+	{
+		free(buffer);
+		close(file_descriptor);
+		return (0);
+	}
 
-    free(buffer);
-    close(file_descriptor);
-    return (bytes_written);
+	free(buffer);
+	close(file_descriptor);
+	return (bytes_written);
 }
